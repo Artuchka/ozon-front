2ff50c8f3bfadb03ebe24072e2c3b8c1933af229
JohnDoeee <yandex949@gmail.com>
2022-12-18 14:37:35 +0300

    my/main html layout done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Default } from "./layouts/Default"
 import { Landing } from "./pages/Landing"
 import "./styles/main.scss"
+import { Personal } from "./pages/Personal"
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Default />}>
 					<Route index element={<Landing />} />
+					<Route path="/my/main" element={<Personal />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
---
+++ src/pages/Personal.tsx
@@ -0,0 +1,64 @@
+import React from "react"
+import { BiFace } from "react-icons/bi"
+
+export const Personal = () => {
+	const avatar = "/"
+	return (
+		<div className="personal-page">
+			<div className="main-info">
+				<div className="avatar">
+					{avatar == "/" ? (
+						<BiFace className="placeholder" />
+					) : (
+						<img src="/" alt="avatar" />
+					)}
+				</div>
+				<div className="desc">
+					<div className="full-name">Горбунов Артем</div>
+					<div className="submain">
+						<div className="bday">22 Марта 1998</div>
+						<div className="gender">Пол не указан</div>
+					</div>
+					<button type="button" className="change">
+						Изменить
+					</button>
+				</div>
+			</div>
+
+			<div className="all-info">
+				<h2>Учётные данные</h2>
+				<small className="tip">
+					Здесь вы можете отредактировать информацию о себе и добавить
+					недостающую
+				</small>
+				<div className="grid">
+					<div className="grid-item">
+						<small className="title">ФИО</small>
+						<div className="value">+79210681264</div>
+						<button className="change">Изменить</button>
+					</div>
+					<div className="grid-item">
+						<small className="title">телефон</small>
+						<div className="value">+79210681264</div>
+						<button className="change">Изменить</button>
+					</div>
+					<div className="grid-item">
+						<small className="title">дата рождения</small>
+						<div className="value">22 марта 1998</div>
+						<button className="change">Изменить</button>
+					</div>
+					<div className="grid-item">
+						<small className="title">почта</small>
+						<div className="value">yandex949@gmail.com</div>
+						<button className="change">Изменить</button>
+					</div>
+				</div>
+			</div>
+
+			<div className="actions">
+				<button className="change">Выйти из аккаунта</button>
+				<button className="change">Удалить аккаунт</button>
+			</div>
+		</div>
+	)
+}
