254d55ef2a3893e23bb2da8b9e305c3bb0d1ee40
JohnDoeee <yandex949@gmail.com>
2022-12-24 19:55:59 +0300

    changed api url to `render`  server

--- src/axios/customFetch.ts
+++ src/axios/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// const ozonAPIURL = "https://ozon-server.onrender.com/api/v1"
-const ozonAPIURL = "http://localhost:3000/api/v1"
+const ozonAPIURL = "https://ozon-server.onrender.com/api/v1"
+// const ozonAPIURL = "http://localhost:3000/api/v1"
 console.log(ozonAPIURL)
 
 export const ozonAPI = axios.create({
