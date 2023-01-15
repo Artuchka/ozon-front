bf0ef9a3c44c40f731634f3522580ad8d2ad675c
Temka Artemka <yandex949@gmail.com>
2023-01-15 21:52:03 +0300

    feature: SingleRange input created

---
+++ src/components/pageBlocks/inputs/SingleRange/index.tsx
@@ -0,0 +1,56 @@
+import React, {
+	ChangeEvent,
+	ChangeEventHandler,
+	FC,
+	useCallback,
+	useRef,
+	useState,
+} from "react"
+
+import styles from "./style.module.scss"
+import { useEffect } from "react"
+import { RangeType } from "../../../Filters"
+import { debounce } from "lodash"
+import { colorThemeType } from "../../../VideoPlayer"
+// import debounce from "lodash.debounce"
+
+type proptype = {
+	name: string
+	min: number
+	max: number
+	value: number
+	onChange: ChangeEventHandler<HTMLInputElement>
+	colorTheme?: colorThemeType
+	className?: string
+}
+
+export const SingleRange: FC<proptype> = ({
+	name = "price",
+	onChange,
+	value,
+	min = 0,
+	max = 100,
+	colorTheme = null,
+	className,
+}) => {
+	return (
+		<div
+			className={`${styles.range}
+			${colorTheme === "primary" ? styles.primary : ""}
+			${colorTheme === "dark" ? styles.dark : ""}
+			${colorTheme === "light" ? styles.light : ""}
+			${className || ""}
+			`}
+		>
+			<input
+				type="range"
+				value={value}
+				onChange={onChange}
+				name={name}
+				min={min}
+				max={max}
+				className={styles.rangeInput}
+			/>
+		</div>
+	)
+}
