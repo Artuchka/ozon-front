c492dc2e799a16a2dcbca928d5ea6b54df429df9
Temka Artemka <yandex949@gmail.com>
2023-02-03 12:06:33 +0300

    optimization: using woff2, woff primarily

--- src/axios/customFetch.ts
+++ src/axios/customFetch.ts
@@ -4,9 +4,9 @@ import axios from "axios"
 // const ozonAPIURL = "http://localhost:3000/api/v1"
 // const ozonAPIURL = "https://busy-red-zebra-robe.cyclic.app/api/v1"
 
-// export const serverURL = "https://ozon-server.onrender.com"
-export const serverURL = "https://busy-red-zebra-robe.cyclic.app"
-// export const serverURL = "http://localhost:3000"
+// export const serverURL = "https://ozon-server.onrendecr.com"
+// export const serverURL = "https://busy-red-zebra-robe.cyclic.app"
+export const serverURL = "http://localhost:3000"
 
 const ozonAPIURL = serverURL + "/api/v1"
 export const ozonAPI = axios.create({
